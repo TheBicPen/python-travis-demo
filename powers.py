16250a21eb46e025ed479532551262aa1d2eb203
Alex <mashkal2000@gmail.com>
2019-06-21 00:38:58 -0400

    removed unused variable

--- powers.py
+++ powers.py
@@ -8,7 +8,7 @@ def power_of(base: int, num: int) -> bool:
     Returns True if num is an integer exponent of base, and False otherwise
     """
     value = 1
-    for i in range(num):
+    for _ in range(num):
         value *= base
         if value == num:
             return True
