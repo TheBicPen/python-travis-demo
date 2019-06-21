2ef15de70176f07e70dd3678442b99c9029c7c13
Alex <mashkal2000@gmail.com>
2019-06-21 00:32:33 -0400

    changed docstring

--- powers.py
+++ powers.py
@@ -5,7 +5,7 @@ Sample Python project to demonstrate use of Travis-CI
 
 def power_of(base: int, num: int) -> bool:
     """
-    Returns True if num is an integer exponent of base, and False otherwise.
+    Returns True if num is an integer exponent of base, and False otherwise
     """
     value = 1
     for i in range(num):
